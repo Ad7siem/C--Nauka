a229cbfc18e55da7a64fb906e771eab7b85682be
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-01-15 11:31:09 +0100

    5.1

---
+++ 5.10 Zadania do samodzielnego rozwiązania/5.10 Zadania do samodzielnego rozwiązania/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._10_Zadania_do_samodzielnego_rozwiązania
+{
+    class Program
+    {
+        static void Temperatura(double x)
+        {
+            double wynik;
+            wynik = ((5.0 / 9.0) * (x - 32.0));
+            Console.WriteLine("{0}F = {1}C", x, wynik);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tZadanie 5.1\n");
+            Console.WriteLine("Proszę podać temperature w stopniach Fahrenheita: ");
+            double f = double.Parse(Console.ReadLine());
+            Temperatura(f);
+            
+            Console.ReadKey();
+        }
+    }
+}
