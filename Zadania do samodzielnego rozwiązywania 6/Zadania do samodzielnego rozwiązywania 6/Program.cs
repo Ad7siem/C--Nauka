85dfd0bb3dc73298af228c7125aededea64f730f
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-01-22 17:20:26 +0100

    zadanie 6.4

--- Zadania do samodzielnego rozwiązywania 6/Zadania do samodzielnego rozwiązywania 6/Program.cs
+++ Zadania do samodzielnego rozwiązywania 6/Zadania do samodzielnego rozwiązywania 6/Program.cs
@@ -50,10 +50,36 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
             
         }
     }
+    class Energia
+    {
+        private int StanPoczatkowy;
+        private int StanAktualny;
+        public Energia(int SP, int SA)
+        {
+            StanPoczatkowy = SP;
+            StanAktualny = SA;
+        }
+        public void Wypisz()
+        {
+            Console.WriteLine("Stan poczatkowy wynosi = {0}, a stan aktualny = {1}", StanPoczatkowy, StanAktualny);
+        }
+        public void zuzyta()
+        {
+            Console.WriteLine("Zużyto {0} energii", StanAktualny - StanPoczatkowy);
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+             * Zadanie 6.1. 
+                Napisz program, który tworzy klasę Prostokat, zawierającą dwie prywatne dane 
+                składowe: dlugosc, szerokosc, dwie prywatne metody: powierzchnia(), obwod() oraz metodę 
+                publiczną – Prezentuj() (która wyświetla powierzchnię i obwód prostokąta) i konstruktor 
+                inicjalizujący. W metodzie Main() zdefiniuj obiekt i uruchom dla niego metodę Prezentuj()
+             */
             Console.WriteLine("\tZadanie 1\n");
             Prostokat p1 = new Prostokat(2,3);
             p1.Prezentuj();
@@ -61,6 +87,12 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
             p2 = new Prostokat(4, 6);
             p2.Prezentuj();
 
+            /*
+             * Zadanie 6.2. 
+                Uzupełnij program z poprzedniego zadania o definicje tablicy obiektów dla 
+                prostokątów. W metodzie Main() wyświetl powierzchnie oraz obwód wszystkich prostokątów 
+                w tablicy używając (wewnątrz pętli foreach) metody publicznej Prezentuj(). 
+             */
             Console.WriteLine("\n\tZadanie 2\n");
             Prostokat[] tab = new Prostokat[3];
             tab[0] = new Prostokat (2, 6);
@@ -69,8 +101,21 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
             foreach(Prostokat p in tab)
             {
                 p.Prezentuj();
-                p.Wypisz();
+                //p.Wypisz();
             }
+
+            /*
+             * Zadanie 6.4. 
+                Zdefiniuj klasę, która pozwoli na rejestrację zużycia energii elektrycznej. Klasa powinna pozwalać na: 
+                 rejestrację początkowego i bieżącego stanu licznika energii,  
+                 uzyskanie danych o początkowym oraz bieżącym stanie licznika, 
+                 obliczanie zużytej energii.
+             */
+
+            Console.WriteLine("\n\tZadanie 4\n");
+            Energia licznik = new Energia(4, 98);
+            licznik.Wypisz();
+            licznik.zuzyta();
 
             Console.ReadKey();
         }
