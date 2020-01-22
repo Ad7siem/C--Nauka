a9d5251dec55281ec157f400562792ddba0f4c32
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-01-22 17:01:59 +0100

    6.3

--- Zadania do samodzielnego rozwiązywania 6/Zadania do samodzielnego rozwiązywania 6/Program.cs
+++ Zadania do samodzielnego rozwiązywania 6/Zadania do samodzielnego rozwiązywania 6/Program.cs
@@ -9,7 +9,7 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
     class Prostokat
     {
         private int dlugosc { get; set; }
-        private int szerokosc;
+        private int szerokosc { get; set; }
         private int obwod()
         {
             return (2 * dlugosc) + (2 * szerokosc);
@@ -26,6 +26,28 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
         public void Prezentuj()
         {
             Console.WriteLine("Obwód prostokąta wynosi {0, 4}, a powierzchnia wynosi {1, 4}", obwod(), powierzchnia());
+        }
+        public void Wypisz()
+        {
+            Console.WriteLine("dlugosc = {0}, szerokosc = {1}", dlugosc, szerokosc);
+        }
+        /*
+         * Zadanie 6.3. 
+            Uzupełnij program z poprzedniego zadania o definicję metody statycznej, która podaje powierzchnię największego prostokąta.
+         */
+        public static void NajwiekszyProstokat(Prostokat[] tab)
+        {
+            int najwiekszy = 0;
+            int pole;
+            for(int i = 0; i < tab.Length; i++)
+            {
+                pole = 2 * 4;
+                if (pole > najwiekszy)
+                {
+                    najwiekszy = pole;
+                }
+            }
+            
         }
     }
     class Program
@@ -47,7 +69,9 @@ namespace Zadania_do_samodzielnego_rozwiązywania_6
             foreach(Prostokat p in tab)
             {
                 p.Prezentuj();
+                p.Wypisz();
             }
+
             Console.ReadKey();
         }
     }
