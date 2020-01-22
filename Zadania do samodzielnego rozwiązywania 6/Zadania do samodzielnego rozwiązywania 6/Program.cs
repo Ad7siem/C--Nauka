b1f8c24325b1fa126a3bd90c4ed68f4cbed0f72c
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-01-22 15:28:37 +0100

    Zadanie 6.1

---
+++ Zadania do samodzielnego rozwiązywania 6/Zadania do samodzielnego rozwiązywania 6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadania_do_samodzielnego_rozwiązywania_6
+{
+    class Prostokat
+    {
+        private int dlugosc { get; set; }
+        private int szerokosc;
+        private int obwod()
+        {
+            return (2 * dlugosc) + (2 * szerokosc);
+        }
+        private int powierzchnia()
+        {
+            return (dlugosc * szerokosc);
+        }
+        public Prostokat(int dl, int sze)
+        {
+            dlugosc = dl;
+            szerokosc = sze;
+        }
+        public void Prezentuj()
+        {
+            Console.WriteLine("Obwód prostokąta wynosi {0, 4}, a powierzchnia wynosi {1, 4}", obwod(), powierzchnia());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat p1 = new Prostokat(2,3);
+            p1.Prezentuj();
+            Prostokat p2;
+            p2 = new Prostokat(4, 6);
+            p2.Prezentuj();
+            Console.ReadKey();
+        }
+    }
+}
