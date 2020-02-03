18dc09b291e537360c0907850aa687615d66c9e0
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-02-03 16:35:49 +0100

    Drog&Drap

--- DragAndDrop/DragAndDrop/MainWindow.xaml.cs
+++ DragAndDrop/DragAndDrop/MainWindow.xaml.cs
@@ -26,17 +26,24 @@ namespace DragAndDrop
             InitializeComponent();
         }
 
+        Point? positionStarting = null;
+
         private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            ListBox lbSender = sender as ListBox;
-            ListBoxItem movedItem = lbSender.GetItemAt(e.GetPosition(lbSender));
-            if(movedItem != null)
-            {
-                DataObject dane = new DataObject();
-                dane.SetData("Format_Lista", lbSender);
-                dane.SetData("Format_ElementyListy", movedItem);
-                DragDrop.DoDragDrop(lbSender, dane, DragDropEffects.Copy | DragDropEffects.Move);
-            }
+            positionStarting = e.GetPosition(this);
+
+            //ListBox lbSender = sender as ListBox;
+            //ListBoxItem movedItem = lbSender.GetItemAt(e.GetPosition(lbSender));
+            //if(movedItem != null)
+            //{
+            //    DataObject dane = new DataObject();
+            //    //dane.SetData("Format_Lista", lbSender);
+            //    //dane.SetData("Format_ElementyListy", movedItem);
+            //    dane.SetData(DataFormats.StringFormat, movedItem.Content as string);
+            //    DragDrop.DoDragDrop(lbSender, dane, DragDropEffects.Copy | DragDropEffects.Move);
+            //    if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+            //        lbSender.Items.Remove(movedItem);
+            //}
         }
 
         private void ListBox_DragEnter(object sender, DragEventArgs e)
@@ -45,5 +52,61 @@ namespace DragAndDrop
                 e.Effects = DragDropEffects.Copy;
             else e.Effects = DragDropEffects.Move;
         }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            /*
+            ListBox lbSource = e.Data.GetData("Format_Lista") as ListBox;
+            ListBoxItem movedItem = e.Data.GetData("Format_ElementyListy") as ListBoxItem;
+
+            if (!e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                lbSource.Items.Remove(movedItem);
+            else movedItem = new ListBoxItem() { Content = movedItem.Content };
+            */
+
+            //string etivisitMovedItem = e.Data.GetData(DataFormats.StringFormat) as string;
+            //ListBoxItem movedItem = new ListBoxItem() { Content = etivisitMovedItem };
+
+            int indeks = lbSender.IndexFromPoint(e.GetPosition(lbSender));
+            string[] etivisitMovedItems = (e.Data.GetData(DataFormats.StringFormat) as string).TrimEnd('\n').Split('\n');
+            foreach(string etivisitMovedItem in etivisitMovedItems)
+            {
+                ListBoxItem movedItem = new ListBoxItem() { Content = etivisitMovedItem };
+                if (indeks < 0) lbSender.Items.Add(movedItem);
+                else lbSender.Items.Insert(indeks++, movedItem);
+            }
+        }
+
+
+        private void ListBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point positionCurrent = e.GetPosition(this);
+            if (e.LeftButton == MouseButtonState.Released || positionStarting.HasValue)
+                return;
+
+            Vector Offset = positionCurrent - positionStarting.Value;
+            if (Math.Abs(Offset.X) > SystemParameters.MinimumHorizontalDragDistance || Math.Abs(Offset.Y) > SystemParameters.MinimumVerticalDragDistance)
+            {
+                ListBox lbSender = sender as ListBox;
+                ListBoxItem movedItem = lbSender.SelectedItem as ListBoxItem;
+                if(movedItem != null)
+                {
+                    string etivisitMovedItems = "";
+                    foreach (ListBoxItem item in lbSender.SelectedItems)
+                        etivisitMovedItems += item.Content as string + "\n";
+                    DataObject dane = new DataObject(DataFormats.StringFormat, etivisitMovedItems);
+
+                    //DataObject dane = new DataObject(DataFormats.StringFormat, movedItem.Content as string);
+                    DragDrop.DoDragDrop(lbSender, dane, DragDropEffects.Copy | DragDropEffects.Move);
+                    positionStarting = null;
+
+                    if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Control))
+                        for (int i = lbSender.SelectedItems.Count - 1; i >= 0; i--)
+                            lbSender.Items.Remove(lbSender.SelectedItems[i]);
+                }
+            }
+        }
+         
     }
 }
