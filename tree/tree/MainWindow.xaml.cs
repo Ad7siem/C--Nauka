6e27bde4239ec3dcf39c79c853fee4cc58875ec7
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-02-04 19:30:28 +0100

    tree

---
+++ tree/tree/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tree
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Przenoszenie okna
+
+        private bool orMoving = false;
+        private Point posiotionStarting;
+        private Cursor cursor;
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ButtonState == Mouse.LeftButton)
+            {
+                orMoving = true;
+                cursor = Cursor;
+                Cursor = Cursors.Hand;
+                posiotionStarting = e.GetPosition(this);
+            }
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (orMoving)
+            {
+                Vector Offset = e.GetPosition(this) - posiotionStarting;
+                Left += Offset.X;
+                Top += Offset.Y;
+            }
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (orMoving)
+            {
+                Cursor = cursor;
+                orMoving = false;
+            }
+        }
+        #endregion
+
+        #region Zamykanie okna
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) this.Close();
+        }
+
+        bool completedDisappearAnimation = false;
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!completedDisappearAnimation)
+            {
+                Storyboard storyboardScrapingWindow = this.Resources["storyboardScrapingWindow"] as Storyboard;
+                storyboardScrapingWindow.Begin();
+                e.Cancel = true;
+            }
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            completedDisappearAnimation = true;
+            Close();
+        }
+        #endregion
+    }
+}
---
+++ tree/tree/iconInTheTray.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tree
+{
+    public class iconInTheTray
+    {
+        private NotifyIcon notifyIcon;
+        private System.Windows.Window window;
+
+        public iconInTheTray(System.Windows.Window window)
+        {
+            //ikona
+            string nameIcon = "choinka.ico";
+            string nameApk = Application.ProductName;
+            System.Windows.Resources.StreamResourceInfo sri = System.Windows.Application.GetResourceStream(new Uri(@"/" + nameApk + ";component/" + nameIcon, UriKind.RelativeOrAbsolute));
+            Icon icon = new Icon(sri.Stream);
+
+            //menu
+            ContextMenuStrip menu = createMenu();
+
+            //ikona w zasobniku
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = icon;
+            notifyIcon.Text = "Choinka " + DateTime.Now.Year.ToString();
+            notifyIcon.ContextMenuStrip = menu;
+            notifyIcon.Visible = true;
+
+            notifyIcon.DoubleClick += (s, e) =>
+            {
+                int howManyDaysToTheHolidays = (new DateTime(DateTime.Today.Year, 12, 24) - DateTime.Now).Days;
+                notifyIcon.BalloonTipTitle = notifyIcon.Text;
+                notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon.BalloonTipText = "Do świąt pozostało " + howManyDaysToTheHolidays + " dni";
+                notifyIcon.ShowBalloonTip(3000);
+            };
+
+            //menu aplikacji
+            this.window = window;
+            window.MouseRightButtonDown += (s, e) =>
+            {
+                System.Windows.Point p = window.PointToScreen(e.GetPosition(window));
+                menu.Show((int)p.X, (int)p.Y);
+            };
+        }
+
+        public bool Visible
+        {
+            get
+            {
+                return notifyIcon.Visible;
+            }
+            set
+            {
+                notifyIcon.Visible = value;
+            }
+        }
+
+        private ContextMenuStrip createMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            ToolStripMenuItem hideToolStripMenuItem = new ToolStripMenuItem("Ukryj");
+            hideToolStripMenuItem.Click += (s, e) => { window.Hide(); };
+            menu.Items.Add(hideToolStripMenuItem);
+
+            ToolStripMenuItem restoreToolStripMenuItem = new ToolStripMenuItem("Przywróć");
+            restoreToolStripMenuItem.Click += (s, e) => { window.Show(); };
+            menu.Items.Add(restoreToolStripMenuItem);
+
+            ToolStripMenuItem closeToolStripMenuItem = new ToolStripMenuItem("Zamknij");
+            closeToolStripMenuItem.Click += (s, e) => { window.Close(); };
+            menu.Items.Add(closeToolStripMenuItem);
+
+            menu.Items.Add(new ToolStripSeparator());
+
+            ToolStripMenuItem aboutTheAutorToolStripMenuItem = new ToolStripMenuItem("O...");
+            aboutTheAutorToolStripMenuItem.Click += (s, e) =>
+            {
+                System.Windows.SplashScreen splashScreen = new System.Windows.SplashScreen("SplashScreen.png");
+                splashScreen.Show(false, true);
+                System.Threading.Thread.Sleep(1000);
+                splashScreen.Close(new TimeSpan(0, 0, 1));
+            };
+            menu.Items.Add(aboutTheAutorToolStripMenuItem);
+            return menu;
+        }
+        public void Remove()
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+    }
+}
