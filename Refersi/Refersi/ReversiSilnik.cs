752b246b9e13850a9d563512d5bbf8dbad20fcda
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-02-14 14:55:02 +0100

    gra

---
+++ Refersi/Refersi/ReversiSilnik.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refersi
+{
+    class ReversiSilnik
+    {
+        public int SzerokoscPlanszy { get; private set; }
+        public int WysokoscPlanszy { get; private set; }
+
+        private int[,] plansza;
+        public int NumerGraczaWykonujacegoNastepnyRuch { get; private set; } = 1;
+
+        private static int numerPrzeciwnika(int numerGracza)
+        {
+            return (numerGracza == 1) ? 2 : 1;
+        }
+
+        private bool czyWspolrzednePolaPrawidlowe(int poziom, int pionowo)
+        {
+            return poziom >= 0 && poziom < SzerokoscPlanszy && pionowo >= 0 && pionowo < WysokoscPlanszy;
+        }
+
+        public int PobierzStanPola(int poziomo, int pionowo)
+        {
+            if (!czyWspolrzednePolaPrawidlowe(poziomo, pionowo))
+                throw new Exception("Nieprawidłowe wspórzędne pola");
+            return plansza[poziomo, pionowo];
+        }
+    }
+}
