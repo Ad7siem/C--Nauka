dfc93d276352a83634b8c27b784f86c2ce071cdf
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-02-17 15:29:20 +0100

    Kalkulator

--- kalkulator/kalkulator/Program.cs
+++ kalkulator/kalkulator/Program.cs
@@ -2,46 +2,6 @@
 
 namespace kalkulator
 {
-    class Kalkulator
-    {
-        public double first { get; private set; }
-        public double second { get; private set; }
-        public string sign { get; private set; }
-        public Kalkulator(double numberFirst, double numberSecond, string mathematicalSign)
-        {
-            first = numberFirst;
-            second = numberSecond;
-            sign = mathematicalSign;
-        }
-        public double Sum()
-        {
-            return first + second ;
-        }
-        public double Difference()
-        {
-            return first - second;
-        }
-        public double Product()
-        {
-            return first * second;
-        }
-        public double Quotient()
-        {
-            return first / second;
-        }
-        public static double Result(Kalkulator k)
-        {
-            if (k.sign == "+")
-                return k.Sum();
-            else if (k.sign == "-")
-                return k.Difference();
-            else if (k.sign == "*")
-                return k.Product();
-            else if (k.sign == "/")
-                return k.Quotient();
-            return 0;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -55,10 +15,10 @@ namespace kalkulator
                 double second = double.Parse(Console.ReadLine());
                 Console.Write("Podaj znak matematyczny: ");
                 string sign = Console.ReadLine();
-                Kalkulator k = new Kalkulator(first, second, sign);
-                Console.WriteLine("{1} {3} {2} = {0}", Kalkulator.Result(k), k.first, k.second, k.sign);
+                calculator k = new calculator(first, second, sign);
+                Console.WriteLine("{1} {3} {2} = {0}", calculator.Result(k), k.first, k.second, k.sign);
                 Console.Write("Czy kontynuować?(T/N): ");
-                or = Console.ReadLine();
+                or = Console.ReadLine().ToUpper();
             } while (or == "T");
         }
     }
---
+++ kalkulator/kalkulator/calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    class calculator
+    {
+        public double first { get; private set; }
+        public double second { get; private set; }
+        public string sign { get; private set; }
+        public calculator(double numberFirst, double numberSecond, string mathematicalSign)
+        {
+            first = numberFirst;
+            second = numberSecond;
+            sign = mathematicalSign;
+        }
+        public double Sum()
+        {
+            return first + second;
+        }
+        public double Difference()
+        {
+            return first - second;
+        }
+        public double Product()
+        {
+            return first * second;
+        }
+        public double Quotient()
+        {
+            return first / second;
+        }
+        public static double Result(calculator k)
+        {
+            if (k.sign == "+")
+                return k.Sum();
+            else if (k.sign == "-")
+                return k.Difference();
+            else if (k.sign == "*")
+                return k.Product();
+            else if (k.sign == "/")
+                return k.Quotient();
+            return 0;
+        }
+    }
+}
