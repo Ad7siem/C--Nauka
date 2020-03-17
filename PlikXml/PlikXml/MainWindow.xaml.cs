a681529bdf4b299f8533dccdab7a88df345570ef
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-03-17 17:34:44 +0100

    XML - błąd w odczytywaniu

---
+++ PlikXml/PlikXml/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+
+
+namespace PlikXml
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (this.ReadWindowPositionAndTitleFromXmlFile(sciezkaPliku))
+                tbTytuł.Text = this.Title;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox) Title = (sender as TextBox).Text;
+        }
+
+        private const string sciezkaPliku = "Ustawienia.xml";
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.SaveWindowPositionAndTitleToXmlFile(sciezkaPliku);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            XDocument xml = XDocument.Load("Ustawienia.xml");
+
+            // wersja XML
+            string wersja = xml.Declaration.Version;
+            MessageBox.Show("Wersja XML: " + wersja);
+
+            // odczytywanie nazwy głownego elementu
+            string nazwaElementuGlownego = xml.Root.Name.LocalName;
+            MessageBox.Show("Nazwa elementu głównego: " + nazwaElementuGlownego);
+
+            // kolekcja podelementów ze wszystkich poziomów drzewa
+            IEnumerable<XElement> wszystkiePodelementy = xml.Root.Descendants();
+            string s = "Wszystkie podelementy: \n";
+            foreach (XElement podelement in wszystkiePodelementy) s += podelement.Name + "\n";
+            MessageBox.Show(s);
+
+            // kolekcja podelementów elementu okna
+            IEnumerable<XElement> podelementyOkna = xml.Root.Element("okno").Elements();
+            s = "Podelementy elementu okno: \n";
+            foreach (XElement podelement in podelementyOkna) s += podelement.Name + "\n";
+            MessageBox.Show(s);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //treeView.PopulateTreeViewWithXmlFile(sciezkaPliku);
+                XDocument xml = XDocument.Load("htttp://www.nbp.pl/kursy/xml/LastC.xml");
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("Błąd poczas odczytywania pliku XML: \n" + exc.Message, Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
---
+++ PlikXml/PlikXml/XmlHelper.cs
@@ -0,0 +1,127 @@
+﻿using System.Windows;
+using System.Xml.Linq;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+
+namespace PlikXml
+{
+    public static class XmlHelper
+    {
+        public static void SaveWindowPositionAndTitleToXmlFile(this Window window, string filePath)
+        {
+            //XDocument xml = new XDocument(
+            //    new XDeclaration("1.0", "utf-8", "yes"),
+            //    new XComment("Window position and title"),
+            //    new XElement("parameters",
+            //        new XElement("window",
+            //            new XAttribute("title", window.Title),
+            //            new XElement("position",
+            //                new XElement("X", window.Left),
+            //                new XElement("Y", window.Top)
+            //        ),
+            //        new XElement("size",
+            //            new XElement("width", window.Width),
+            //            new XElement("height", window.Height)
+            //            )
+            //        )
+            //    )
+            //);
+
+            //xml.Save(filePath);   
+            
+            // definiowanie obiektów
+            XDeclaration declaration = new XDeclaration("1.0", "utf-8", "yes");
+            XComment comment = new XComment("Window posiotion and title");
+            XElement parameters = new XElement("parameters");
+            XElement _window = new XElement("window");
+            XAttribute title = new XAttribute("title", window.Title);
+            XElement posiotion = new XElement("position");
+            XElement X = new XElement("X", window.Left);
+            XElement Y = new XElement("Y", window.Top);
+            XElement size = new XElement("size");
+            XElement width = new XElement("width", window.Width);
+            XElement height = new XElement("height", window.Height);
+
+            // budowanie drzewa (od gałęzi)
+            posiotion.Add(X);
+            posiotion.Add(Y);
+            size.Add(width);
+            size.Add(height);
+            _window.Add(title);
+            _window.Add(posiotion);
+            _window.Add(size);
+            parameters.Add(_window);
+
+            XDocument xml = new XDocument();
+            xml.Declaration = declaration;
+            xml.Add(comment);
+            xml.Add(parameters);
+
+            // zapis do pliku
+            xml.Save(filePath);
+        }
+
+        public static bool ReadWindowPositionAndTitleFromXmlFile(this Window window, string filePath)
+        {
+            try
+            {
+                XDocument xml = XDocument.Load(filePath);
+
+                // odczytanie tytułu okna
+                window.Title = xml.Root.Element("Window").Attribute("title").Value;
+
+                // odczytanie pozycji i wielkość
+                XElement pozycja = xml.Root.Element("Window").Element("posiotion");
+                window.Left = double.Parse(pozycja.Element("X").Value);
+                window.Top = double.Parse(pozycja.Element("Y").Value);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #region TreeView
+        private static void AddElementToNode(XElement elementXml, TreeViewItem treeNode, int level)
+        {
+            level++;
+
+            IEnumerable<XElement> elements = elementXml.Elements();
+            foreach (XElement element in elements)
+            {
+                string description = element.Name.LocalName;
+                if (!element.HasElements && !string.IsNullOrEmpty(element.Value))
+                    description += $" ({element.Value.Trim(' ', '\n')})";
+                TreeViewItem newNode = new TreeViewItem()
+                {
+                    Header = description,
+                    IsExpanded = true                
+                };
+            treeNode.Items.Add(newNode);
+            AddElementToNode(elementXml, newNode, level);
+            }
+        }
+
+        public static void PopulateTreeViewWithXmlFile(this TreeView treeView, string filePath)
+        {
+            treeView.Items.Clear();
+
+            XDocument xml = XDocument.Load(filePath);
+            TreeViewItem rootNode = new TreeViewItem()
+            {
+                Header = xml.Root.Name.LocalName,
+                IsExpanded = true
+            };
+            treeView.Items.Add(rootNode);
+
+            treeView.BeginInit();
+            AddElementToNode(xml.Root, rootNode, 0);
+            treeView.EndInit();
+        }
+        #endregion
+    }
+
+}
