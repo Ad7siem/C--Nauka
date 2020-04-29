6634e15d2fb2a83c489101ced44f89d2172ee1d5
Ad7siem <53908369+Ad7siem@users.noreply.github.com>
2020-04-29 19:44:50 +0200

    trochę

---
+++ XamlWpfCustomControl/Przyciski.xaml/CustomControl1.vb
@@ -0,0 +1,10 @@
+﻿Public Class CustomControl1
+    Inherits Control
+
+    Shared Sub New()
+        'To wywołanie metody OverrideMetadata informuje system, że ten element chce dostarczyć styl, który jest inny niż jego klasa bazowa.
+        'Ten styl jest zdefiniowany w klasie Themes\Generic.xaml
+        DefaultStyleKeyProperty.OverrideMetadata(GetType(CustomControl1), New FrameworkPropertyMetadata(GetType(CustomControl1)))
+    End Sub
+
+End Class
---
+++ XamlWpfCustomControl/Przyciski/PrzyciskiUMK.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Przyciski
+{
+    public class PrzyciskiUMK : Button
+    {
+        static PrzyciskiUMK()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PrzyciskiUMK), new FrameworkPropertyMetadata(typeof(PrzyciskiUMK)));
+        }
+    }
+}
